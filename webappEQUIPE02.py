f10adaf24a0e29be8299a166f8e7b7d9cdfb023a
lpgroup <68185745+lpgroup@users.noreply.github.com>
2022-09-18 08:11:48 -0300

    Update webappEQUIPE02.py

--- webappEQUIPE02.py
+++ webappEQUIPE02.py
@@ -42,13 +42,13 @@ st.sidebar.info("By: Prof. Massaki de O. Igarashi")
 if choice == "Dúvidas": 
     st.header("Relatório de DÚVIDAS")   
     st.write('EQUIPE 02:')
-    st.info('Dúvida(s) Enviada(s)')
+    st.warning('Dúvida(s) Enviada(s)')
     st.code(df02D['duvida']) 
            
 elif choice == "Respostas":       
     st.header("Relatório de RESPOSTAS")    
     st.write('EQUIPE 02:')    
-    st.warning('Resposta do(a) TUTOR(A):')
+    st.info('Resposta do(a) TUTOR(A):')
     st.code(df02R['resposta'])  
 
                
@@ -62,7 +62,7 @@ elif choice == "Dúvidas e Respostas":
         st.write("Nº TOTAL de dúvidas RESPONDIDAS:")
         st.info(NregDf02R)
     st.subheader('EQUIPE 01:')
-    st.info('Dúvida(s) Enviada(s)')
+    st.warning('Dúvida(s) Enviada(s)')
     st.code(df02D['duvida']) 
-    st.warning('Resposta do(a) TUTOR(A):')
+    st.info('Resposta do(a) TUTOR(A):')
     st.code(df02R['resposta'])  
